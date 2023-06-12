3d2f98c8bbec40701d1273296dd860dbb4e29896
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2023-06-12 15:22:07 -0300

    Exercicio Aula 02
    Exercício sobre argumentos

--- exercicioAula2/Argumentos02.java
+++ exercicioAula2/Argumentos02.java
@@ -2,6 +2,6 @@ package exercicioAula2;
 
 public class Argumentos02 {
     public static void main(String[] args){
-        System.out.println("Olá Mundo" + args[0]);
+        System.out.println("Olá Mundo " + args[0]);
     }
 }
