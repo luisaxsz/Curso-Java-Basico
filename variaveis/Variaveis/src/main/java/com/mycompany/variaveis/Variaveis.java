142b2a8c10cd3f60e91630f64f2be23cd16a4d78
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2023-06-13 16:24:14 -0300

    Variáveis de tipo primitivo
    Aula sobre declaração de variáveis em java

---
+++ variaveis/Variaveis/src/main/java/com/mycompany/variaveis/Variaveis.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.variaveis;
+
+/**
+ *
+ * @author maria
+ */
+public class Variaveis {
+
+    public static void main(String[] args) {
+        byte idade01 = 20;
+        short idade02 = 21;
+        int idade03 = 32;
+        long idade04 = 23;
+        
+        System.out.println("Idade 1: " + idade01);
+        System.out.println("Idade 2: " + idade02);
+        System.out.println("Idade 3: " + idade03);
+        System.out.println("Idade 4: " + idade04);
+        
+        double valorPassagem = 2.50;
+        float valorTomate = 3.45f;
+        
+        System.out.println("Valor passagem: " + valorPassagem);
+        System.out.println("Valor tomate: " + valorTomate);
+        
+        char o = 111;
+        char i = 105;
+        
+        System.out.println("" + o + i);
+    }
+}
