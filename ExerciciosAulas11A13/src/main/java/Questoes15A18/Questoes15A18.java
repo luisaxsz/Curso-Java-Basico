ea7d42eca57091237c2eacf13e6954efc5ee0a76
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2023-06-17 22:06:30 -0300

    questão 15

---
+++ ExerciciosAulas11A13/src/main/java/Questoes15A18/Questoes15A18.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Questoes15A18;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author maria
+ */
+public class Questoes15A18 {
+   public static void main(String[] args){
+       //Scanner
+       Scanner scan = new Scanner(System.in);
+       
+       /*//Questao 15
+       System.out.println("Quantas horas você trabalhou no mês?");
+       double horasTrabalhadas = scan.nextDouble();
+       System.out.println("Quanto você ganha por hora trabalhada?");
+       double valorPorHoraTrabalhada = scan.nextDouble();
+       double salario = valorPorHoraTrabalhada*horasTrabalhadas;
+       System.out.println("Seu salario é de: " + salario);
+       double pagoInss = salario*8/100;
+       double restoSalario = salario - pagoInss;
+       System.out.println("Você pagou ao INSS: " + pagoInss);
+       double pagoImpostoDeRenda = restoSalario*11/100;
+       restoSalario = restoSalario - pagoImpostoDeRenda;
+       System.out.println("Você pagou para o imposto de renda: " + pagoImpostoDeRenda);
+       double pagoSindicato = restoSalario*5/100;
+       restoSalario = restoSalario - pagoSindicato;
+       System.out.println("Você pagou ao Sindicato: " + pagoSindicato);
+       double salarioLiquido = restoSalario;
+       System.out.println("Seu salario liquido é de: " + salarioLiquido);*/
+       
+       //Questao 16 
+       System.out.println("***Loja de tinta***");
+       System.out.println("Qual tamanho da area a ser pintada? ");
+       double areaAPintar = scan.nextDouble();
+       
+               
+       
+       
+       
+   }
+  
+}
