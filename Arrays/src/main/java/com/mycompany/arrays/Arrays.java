e73430d11865e29adec8f07c13fbb8af07cd1a98
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2023-06-22 19:37:32 -0300

    Exercicio Vetores

--- Arrays/src/main/java/com/mycompany/arrays/Arrays.java
+++ Arrays/src/main/java/com/mycompany/arrays/Arrays.java
@@ -3,6 +3,8 @@
  */
 package com.mycompany.arrays;
 
+import java.util.Scanner;
+
 /**
  *
  * @author maria
@@ -10,18 +12,47 @@ package com.mycompany.arrays;
 public class Arrays {
 
     public static void main(String[] args) {
+        //Scanner
+        
+        Scanner scan = new Scanner(System.in);
+        
+        /*//Questao 1 - aula 19
         int inteirosA[] = new int[5];
         int inteirosB[] = new int[5];
 
-        inteirosA[0] = 1;
-        inteirosA[1] = 2;
-        inteirosA[2] = 3;
-        inteirosA[3] = 4;
-        inteirosA[4] = 5;
-
-        for (int i = 0; i < 5; i++) {
+        inteirosA[0] = 10;
+        inteirosA[1] = 20;
+        inteirosA[2] = 30;
+        inteirosA[3] = 40;
+        inteirosA[4] = 50;
+        
+        for (int i = 0; i < inteirosA.length; i++) {
             inteirosB[i] = inteirosA[i];
             System.out.println(inteirosB[i]);
+        }*/
+        
+        //Questao 19 - aula 19
+        double notas1[] = new double [10];
+        double notas2[] = new double [10];
+        double medias[] = new double [10];
+        
+        for (int j = 0 ; j < notas1.length; j++){
+            System.out.println("Digite sua primeira nota: "); 
+            notas1[j] = scan.nextDouble();
+            System.out.println("===========================");
+            System.out.println("Digite sua segunda nota: ");
+            notas2[j] = scan.nextDouble();
+            medias[j] = (notas1[j] + notas2[j])/2;
+            if (medias[j] < 7){
+                System.out.println("Sua media foi: " + medias[j]);
+                System.out.println("Reprovado");
+            }else {
+                System.out.println("Sua media foi: " + medias[j]);
+                System.out.println("Aprovado");
+            }
         }
+        
+        
+        
     }
 }
