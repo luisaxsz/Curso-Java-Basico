3d7c9ee1535af17761155f08c82deac8374373f4
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2023-06-22 14:34:15 -0300

    Ultimas questões do Exercicio

--- ExerciciosAulas11A13/src/main/java/Questoes15A18/Questoes15A18.java
+++ ExerciciosAulas11A13/src/main/java/Questoes15A18/Questoes15A18.java
@@ -32,17 +32,41 @@ public class Questoes15A18 {
        restoSalario = restoSalario - pagoSindicato;
        System.out.println("Você pagou ao Sindicato: " + pagoSindicato);
        double salarioLiquido = restoSalario;
-       System.out.println("Seu salario liquido é de: " + salarioLiquido);*/
+       System.out.println("Seu salario liquido é de: " + salarioLiquido);
        
        //Questao 16 
        System.out.println("***Loja de tinta***");
        System.out.println("Qual tamanho da area a ser pintada? ");
        double areaAPintar = scan.nextDouble();
+       double litros = areaAPintar/3;
+       double latas = Math.round(litros/18);
+       double valorFinal = latas * 80;
+       System.out.println("Você precisa de " + latas + " latas");
+       System.out.println("O valor final é de: R$" + valorFinal);*/  
        
-               
+       //Questão 17 
+       System.out.println("***Loja de tinta***");
+       System.out.println("Qual tamanho da area a ser pintada? ");
+       double areaAPintar02 = scan.nextDouble();
+       double litros02 = areaAPintar02/6;
+       System.out.println("Voce precisa de: " + litros02 + " litros");
+       System.out.println("Para comprar em galões de 3,6 Litros digite [1]");
+       System.out.println("Para comprar em galões de 18 Litros digite [2]");
+       int tipoGalao = scan.nextInt();
        
+       if (tipoGalao == 1){
+        double latas02 = litros02/3.6;
+        double valorFinal02 = latas02 * 25;
+        System.out.println("Você precisa de " + latas02 + " latas");
+        System.out.println("O valor final é de: R$" + valorFinal02);  
+       }else {
+        double latas02 = litros02/18;
+        double valorFinal02 = latas02 * 80;
+        System.out.println("Você precisa de " + latas02 + " latas");
+        System.out.println("O valor final é de: R$" + valorFinal02);  
+       }
        
        
    }
-  
+   
 }
