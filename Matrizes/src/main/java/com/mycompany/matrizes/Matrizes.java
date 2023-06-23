9e72687f739977a13850f5201e2b0f5b8bce7319
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2023-06-23 11:35:56 -0300

    Questão 2 Matrizes - Aula 20

--- Matrizes/src/main/java/com/mycompany/matrizes/Matrizes.java
+++ Matrizes/src/main/java/com/mycompany/matrizes/Matrizes.java
@@ -4,6 +4,7 @@
 package com.mycompany.matrizes;
 
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -11,17 +12,46 @@ import java.util.Random;
  */
 public class Matrizes {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {  
+      //Scanner e Random
+      Random rand = new Random();
+      Scanner scan = new Scanner(System.in);
+      
         //Questão 1 - Aula 20 
         int[][] matriz01 = new int[4][4];
 
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                Random rand = new Random();
                 matriz01[i][j] = rand.nextInt(9) + 0;
                 System.out.print(matriz01[i][j]);
             }
             System.out.println("");
         }
+        
+        //Questao 2 - Aula 20
+        int matriz02[][] = new int [3][3];
+        int countPares = 0;
+        int countImpares = 0; 
+        for (int k = 0 ; k < 3 ; k++){
+            for (int l = 0; l < 3 ; l++){
+                System.out.println("Insira um valor: ");
+                matriz02[k][l] = scan.nextInt();
+                if (matriz02[k][l]%2 == 0){
+                    countPares++;
+                }else {
+                    countImpares++;
+                }
+            }
+        }
+        System.out.println("======Matriz======");
+        for(int i = 0 ; i < matriz02.length; i++){
+            for (int j = 0 ; j < matriz02.length ; j++){
+                System.out.print(matriz02[i][j] + "\t");
+            }
+            System.out.println("");
+        }
+        
+        System.out.println("A quantidade de numeros pares é: " + countPares);
+        System.out.println("A quantidade de numeros impares é: " + countImpares);
     }
 }
