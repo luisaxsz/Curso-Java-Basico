5040eec76918c9a1b8d72189b0faad819aa0f8d6
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2023-06-22 21:32:13 -0300

    Update Arrays.java

--- Arrays/src/main/java/com/mycompany/arrays/Arrays.java
+++ Arrays/src/main/java/com/mycompany/arrays/Arrays.java
@@ -3,6 +3,7 @@
  */
 package com.mycompany.arrays;
 
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -17,6 +18,7 @@ public class Arrays {
         Scanner scan = new Scanner(System.in);
         
         //Questao 1 - aula 19
+        System.out.println("=====Vetores com mesmos valores=====");
         int inteirosA[] = new int[5];
         int inteirosB[] = new int[5];
 
@@ -32,6 +34,7 @@ public class Arrays {
         }
         
         //Questao 19 - aula 19
+        System.out.println("=====Armarzenamento de notas e médias=====");
         double notas1[] = new double [10];
         double notas2[] = new double [10];
         double medias[] = new double [10];
@@ -52,7 +55,40 @@ public class Arrays {
             }
         }
         
+        //Questão 20 - aula 19 
+        System.out.println("=====Conversão reais em dolar=====");
+        double conversao[] = new double[21];
         
+        for (int h = 1; h < conversao.length;h++){
+            conversao[h] = h*4.77;
+            System.out.println("O valor R$" + h + " em dolars é: " + conversao[h]);
+        }
+        
+        //Questao 23 - Aula 19 
+        System.out.println("=====Vetores Pares=====");
+        int numeros[] = new int[10];
+        
+        for (int n = 0 ; n < numeros.length; n++){
+            System.out.println("Digite um numero: ");
+            numeros[n] = scan.nextInt();
+            
+            if(numeros[n]%2 == 0){
+                System.out.println(numeros[n]);
+            }else{
+                break;
+            }
+        }
+        
+        /*//Questao 24 - Aula 16 
+        System.out.println("=====Palídromos=====");
+        int numeros02[] = new int[4];
+        
+        for (int m = 0 ; m < numeros02.length; m++){
+            Random rand = new Random();
+            numeros02[m] = rand.nextInt(500) + 0;*  
+        }*/
+           
+            
         
     }
 }
