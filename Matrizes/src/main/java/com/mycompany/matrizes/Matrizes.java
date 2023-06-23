4b6239c44142c5ba2ea561a409abe5acb38828fe
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2023-06-23 11:36:56 -0300

    formatação

--- Matrizes/src/main/java/com/mycompany/matrizes/Matrizes.java
+++ Matrizes/src/main/java/com/mycompany/matrizes/Matrizes.java
@@ -12,11 +12,11 @@ import java.util.Scanner;
  */
 public class Matrizes {
 
-    public static void main(String[] args) {  
-      //Scanner e Random
-      Random rand = new Random();
-      Scanner scan = new Scanner(System.in);
-      
+    public static void main(String[] args) {
+        //Scanner e Random
+        Random rand = new Random();
+        Scanner scan = new Scanner(System.in);
+
         //Questão 1 - Aula 20 
         int[][] matriz01 = new int[4][4];
 
@@ -27,30 +27,30 @@ public class Matrizes {
             }
             System.out.println("");
         }
-        
+
         //Questao 2 - Aula 20
-        int matriz02[][] = new int [3][3];
+        int matriz02[][] = new int[3][3];
         int countPares = 0;
-        int countImpares = 0; 
-        for (int k = 0 ; k < 3 ; k++){
-            for (int l = 0; l < 3 ; l++){
+        int countImpares = 0;
+        for (int k = 0; k < 3; k++) {
+            for (int l = 0; l < 3; l++) {
                 System.out.println("Insira um valor: ");
                 matriz02[k][l] = scan.nextInt();
-                if (matriz02[k][l]%2 == 0){
+                if (matriz02[k][l] % 2 == 0) {
                     countPares++;
-                }else {
+                } else {
                     countImpares++;
                 }
             }
         }
         System.out.println("======Matriz======");
-        for(int i = 0 ; i < matriz02.length; i++){
-            for (int j = 0 ; j < matriz02.length ; j++){
+        for (int i = 0; i < matriz02.length; i++) {
+            for (int j = 0; j < matriz02.length; j++) {
                 System.out.print(matriz02[i][j] + "\t");
             }
             System.out.println("");
         }
-        
+
         System.out.println("A quantidade de numeros pares é: " + countPares);
         System.out.println("A quantidade de numeros impares é: " + countImpares);
     }
