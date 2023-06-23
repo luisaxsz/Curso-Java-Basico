c1286dece7ea8ca9d912403c494567467e3debd8
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2023-06-23 10:29:18 -0300

    Exercicio Matrizes

--- Matrizes/src/main/java/com/mycompany/matrizes/Matrizes.java
+++ Matrizes/src/main/java/com/mycompany/matrizes/Matrizes.java
@@ -1,8 +1,9 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.matrizes;
+
+import java.util.Random;
 
 /**
  *
@@ -11,6 +12,16 @@ package com.mycompany.matrizes;
 public class Matrizes {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //Questão 1 - Aula 20 
+        int[][] matriz01 = new int[4][4];
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                Random rand = new Random();
+                matriz01[i][j] = rand.nextInt(9) + 0;
+                System.out.print(matriz01[i][j]);
+            }
+            System.out.println("");
+        }
     }
 }
