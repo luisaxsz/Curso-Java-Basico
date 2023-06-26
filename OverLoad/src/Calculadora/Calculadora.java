941c246637fffc98149cadf504b0e2c50d7e8ef1
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2023-06-26 18:13:24 -0300

    Aula 33 - OverLoad

---
+++ OverLoad/src/Calculadora/Calculadora.java
@@ -0,0 +1,20 @@
+package Calculadora;
+
+public class Calculadora {
+	public int soma(int num1, int num2) {
+		return num1 + num2;
+	}
+	
+	public double soma(double num1, double num2) {
+		return num1 + num2;
+	}
+	
+	public int soma(int[] vetorInteiros) {
+		int total = 0;
+		
+		for(int i = 0 ; i < vetorInteiros.length; i++) {
+			total += vetorInteiros[i];
+		}
+		return total;
+	}
+}
---
+++ OverLoad/src/Calculadora/TesteCalculadora.java
@@ -0,0 +1,12 @@
+package Calculadora;
+
+public class TesteCalculadora {
+
+	public static void main(String[] args) {
+		Calculadora calc = new Calculadora();
+
+		System.out.println(calc.soma(1, 2));
+
+	}
+
+}
