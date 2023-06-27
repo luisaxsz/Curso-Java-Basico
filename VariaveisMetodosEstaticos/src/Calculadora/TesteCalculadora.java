6e277f6112722361a0a0c78ff41e00eb5e514bb6
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2023-06-27 12:12:26 -0300

    Aula 34 - Métodos Estáticos

---
+++ VariaveisMetodosEstaticos/src/Calculadora/TesteCalculadora.java
@@ -0,0 +1,12 @@
+package Calculadora;
+
+public class TesteCalculadora {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Calculadora.soma(1.5, 1.5));
+		System.out.println(Calculadora.soma(1,2));
+
+	}
+
+}
