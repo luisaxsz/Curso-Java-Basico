7e5f35595e70ac9653824ae9de6c9bf583293ba2
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2023-06-27 19:06:25 -0300

    Aula 36 - Relacionamento entre classes

---
+++ RelacionamentoClasses/src/RelacionamentoClasses/Endereco.java
@@ -0,0 +1,25 @@
+package RelacionamentoClasses;
+
+public class Endereco {
+	private String nomeRua;
+	private String numero;
+	private String complemento;
+	public String getNomeRua() {
+		return nomeRua;
+	}
+	public void setNomeRua(String nomeRua) {
+		this.nomeRua = nomeRua;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getComplemento() {
+		return complemento;
+	}
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+}
---
+++ RelacionamentoClasses/src/RelacionamentoClasses/TesteContatos.java
@@ -0,0 +1,60 @@
+package RelacionamentoClasses;
+
+public class TesteContatos {
+
+	public static void main(String[] args) {
+		Contato contato = new Contato();
+		
+		contato.setNome("tyrion");
+		
+		//criar objeto endereco
+		Endereco end = new Endereco();
+		
+		end.setNomeRua("caripunas");
+		end.setNumero("n/a");
+		end.setComplemento("n/a");
+		//Setar endereço
+		contato.setEndereco(end);
+		//mostrar valor
+		System.out.println(contato.getEndereco().getComplemento());
+		//Deixar mais seguro 
+		if(contato.getEndereco() != null && contato != null) {
+			System.out.println(contato.getEndereco().getComplemento());
+		}
+		
+		//Setado classe telefone
+		Telefone pessoal = new Telefone();
+		
+		pessoal.setTipo("celular");
+		pessoal.setNumero("9999-9999");
+		pessoal.setDdd("91");
+		
+		//contato.setTelefone(tel);
+		
+		Telefone casa = new Telefone();
+		
+		casa.setTipo("celular");
+		casa.setNumero("7777-7777");
+		casa.setDdd("91");
+		
+		//Colocar em arrray de telefone
+		
+		Telefone[] telefones = new Telefone[2];
+		
+		telefones[0] = pessoal;
+		telefones[1] = casa;
+		
+		contato.setTelefones(telefones);
+		
+		if (contato.getTelefones() != null && contato != null) {
+			for (Telefone tel : contato.getTelefones()) {
+				System.out.println(tel.getNumero());
+			}
+		}
+		
+		
+		
+
+	}
+
+}
